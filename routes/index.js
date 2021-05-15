ce92149465360bc7e8447d06ffff1ffe023e3c98
Satyam Abhishek <satiitmadras123@gmail.com>
2021-05-15 14:38:41 +0530

    adding comment schema

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ console.log('router is loaded');
 
 router.get('/', homeController.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
+router.use('/comments',require('./comments'));
 //for any further routes,accesss from here
 // router.use('/routerName',require('./routerfile'));
 
